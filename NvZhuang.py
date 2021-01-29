1ed4c7c15808fff19b370247f13dceacdf361b5d
MiaoXia0 <953003855@qq.com>
2021-01-29 19:41:35 +0800

    修改config

--- NvZhuang.py
+++ NvZhuang.py
@@ -1,13 +1,13 @@
 from nonebot import MessageSegment
 from hoshino import Service
-import .config
+from .config import *
 
 sv = Service('NvZhuang', enable_on_default=False)
 
 
 @sv.on_keyword('女装')
 async def XuYuNvZhuang(bot, ev):
-    if config.AT:
+    if AT:
         await bot.send(ev, MessageSegment.at(1272483979) + '须臾女装！')
     else:
         await bot.send(ev, '须臾女装！')
